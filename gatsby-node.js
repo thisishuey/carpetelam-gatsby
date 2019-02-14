b0ea2c172c09e9f2909a9167b580c3e71508076e
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-02-14 21:54:24 +0000

    Refactor gatsby-node setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,41 +3,39 @@ const Promise = require("bluebird");
 const path = require("path");
 const slash = require("slash");
 
-const GetWordPressPages = `
-  query GetWordPressPages {
-    allWordpressPage {
-      edges {
-        node {
-          id
-          slug
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const GetWordPressPages = graphql(`
+    query GetWordPressPages {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            link
+            slug
+          }
         }
       }
     }
-  }
-`;
-
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+  `);
   return new Promise((resolve, reject) => {
-    graphql(GetWordPressPages)
-      .then(result => {
-        if (result.errors) {
-          console.log(result.errors);
-          reject(result.errors);
-        }
-        const pageTemplate = path.resolve("./src/templates/Page.js");
-        _.each(result.data.allWordpressPage.edges, ({ node }) => {
-          return createPage({
-            path: node.slug,
-            component: slash(pageTemplate),
-            context: {
-              id: node.id
-            }
-          });
+    GetWordPressPages.then(result => {
+      if (result.errors) {
+        console.log(result.errors);
+        reject(result.errors);
+      }
+      const pageTemplate = path.resolve("./src/templates/Page.js");
+      _.each(result.data.allWordpressPage.edges, ({ node }) => {
+        return createPage({
+          path: node.link,
+          component: slash(pageTemplate),
+          context: {
+            id: node.id
+          }
         });
-      })
-      .then(result => {
-        resolve();
       });
+    }).then(result => {
+      resolve();
+    });
   });
 };
