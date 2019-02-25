4827da51794570a22c0e9bb807af09dcb60d4b10
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-02-25 23:17:47 +0000

    Add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,9 +53,10 @@ module.exports = {
           "**/*/*/themes"
         ]
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    "gatsby-plugin-netlify"
   ]
 };
