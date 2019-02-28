a47e2c10b64e0f7cdf6810c26cd3f561e44bca88
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-02-28 15:33:12 +0000

    Clean up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 import Layout from "../components/Layout";
+import Parallax from "../components/Parallax";
 import SEO from "../components/SEO";
+import withRoot from "../utils/withRoot";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Parallax pageSubtitle="Not Found" pageTitle="404" />
+    <Typography component={Paper} variant="body1">
+      You just hit a route that doesn't exist... the sadness.
+    </Typography>
   </Layout>
 );
 
-export default NotFoundPage;
+export default withRoot(NotFoundPage);
