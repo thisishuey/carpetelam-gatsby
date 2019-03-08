5eacdead7eb4bc54d7ff4458d3d75e2363263589
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-03-08 22:36:13 +0000

    Add cypress and e2e smoke test

---
+++ cypress/e2e/smoke.js
@@ -0,0 +1,32 @@
+describe("app", () => {
+  it("home nav works", () => {
+    cy.visit("/about")
+      .getByText(/home/i)
+      .click()
+      .getByText(/carpe telam/i);
+  });
+  it("about nav works", () => {
+    cy.visit("/")
+      .getByText(/about/i)
+      .click()
+      .getByText(/about me/i);
+  });
+  it("services nav works", () => {
+    cy.visit("/")
+      .getByText(/services/i)
+      .click()
+      .getByText(/my services/i);
+  });
+  it("projects nav works", () => {
+    cy.visit("/")
+      .getByText(/projects/i)
+      .click()
+      .getByText(/my projects/i);
+  });
+  it("contact nav works", () => {
+    cy.visit("/")
+      .getByText(/contact/i)
+      .click()
+      .getByText(/contact me/i);
+  });
+});
