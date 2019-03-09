e1002d20a126eab74d3842049296935311674db1
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-03-09 05:18:57 +0000

    Formatting

--- cypress/e2e/smoke.js
+++ cypress/e2e/smoke.js
@@ -5,24 +5,28 @@ describe("app", () => {
       .click()
       .getByText(/carpe telam/i);
   });
+
   it("about nav works", () => {
     cy.visit("/")
       .getByText(/about/i)
       .click()
       .getByText(/about me/i);
   });
+
   it("services nav works", () => {
     cy.visit("/")
       .getByText(/services/i)
       .click()
       .getByText(/my services/i);
   });
+
   it("projects nav works", () => {
     cy.visit("/")
       .getByText(/projects/i)
       .click()
       .getByText(/my projects/i);
   });
+
   it("contact nav works", () => {
     cy.visit("/")
       .getByText(/contact/i)
